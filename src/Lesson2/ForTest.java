77fbc161c782049867bb3a10f5e61f43c9168c69
Gevorg Bashmaxchyan <gev.bashmaxchyan@mail.ru>
2021-10-04 18:55:39 +0400

    lesson1

--- src/Lesson2/ForTest.java
+++ src/Lesson2/ForTest.java
@@ -6,7 +6,7 @@ public class ForTest {
     public static void main(String[] args) {
         int x;
 
-        for (x = 0; x < 10; x = x + 1) ;
+        for (x = 0; x < 10; x ++)
           System.out.println("kuku x:" + x);
     }
 
