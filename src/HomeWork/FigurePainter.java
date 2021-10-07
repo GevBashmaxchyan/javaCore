1d3623a91904246e2908f2034461e018c15a388e
Gevorg Bashmaxchyan <gev.bashmaxchyan@mail.ru>
2021-10-07 22:14:13 +0400

    lesson4

--- src/HomeWork/FigurePainter.java
+++ src/HomeWork/FigurePainter.java
@@ -2,12 +2,13 @@ package HomeWork;
 
 public class FigurePainter {
     public static void main(String[] args) {
+        int i;
+        char j;
 
-        for (int i = 0; i < 4; i++) {
-            for (int j = 0; j < 1; j++) {
+        for (i = 0; i < 4; i++) {
+            for (j = 0; j < i + 1; j++)
                 System.out.print("* ");
-
-            }
+            System.out.println();
 
 
         }
